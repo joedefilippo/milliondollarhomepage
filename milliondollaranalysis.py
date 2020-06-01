c01a03aafb43284ef3bbb853f97f7e5d114eac2a
joedefilippo <joseph.j.defilippo@gmail.com>
2020-05-31 22:28:53 -0400

    made HTML page with links out of the heatmap.
    Wrote some additional functions for analysis

--- milliondollaranalysis.py
+++ milliondollaranalysis.py
@@ -4,34 +4,34 @@
 import milliondollardata as mdd
 import pprint
 from PIL import Image, ImageDraw
+from collections import Counter
 
 
 def countSitesWithStatusCode(mdhpData, sc):
-    totalSites = len(mdhpData) + 1
     count = 0
-    for i in range(1, totalSites):
+    for i in range(1, len(mdhpData) + 1):
         if mdhpData[i]['status'] == sc:
             count += 1
     return count
 
-def statusCodeSummary(mdhpData):
+def getStatusCodeSummary(mdhpData):
     count = {}
-    totalSites = len(mdhpData) + 1
-
-    for i in range(1, totalSites):
+    for i in range(1, len(mdhpData) + 1):
         count[mdhpData[i]['status']] = 0
-    for i in range(1, totalSites):
+    for i in range(1, len(mdhpData) + 1):
         count[mdhpData[i]['status']] += 1
-
     return count
+
+def getStatusCodeSummaryCounter(mdhpData):
+    #TODO find better way here
+    result = Counter(mdhpData.values())
+    return result
 
 def generateHeatmap(mdhpData):
     im = Image.new('RGBA', (1000, 1000), 'white')
     draw = ImageDraw.Draw(im)
 
-    totalSites = len(mdhpData) + 1
-
-    for i in range(1, totalSites):
+    for i in range(1, len(mdhpData) + 1):
         if mdhpData[i]['status'] == 0:
             draw.rectangle(eval(mdd.millionDollarData[i]['coords']), fill='blue')
         elif mdhpData[i]['status'] == 200:
@@ -40,10 +40,38 @@ def generateHeatmap(mdhpData):
             draw.rectangle(eval(mdd.millionDollarData[i]['coords']), fill='red')
     im.save('heatmap.png')
 
+def getSitesOffline(mdhpData):
+    for i in range(1, len(mdhpData) + 1):
+        if mdhpData[i]['status'] != 0:
+            del mdhpData[i]
+    return mdhpData
+
+def getSitesPendingOrder(mdhpData):
+    for i in range(1, len(mdhpData) + 1):
+        if mdhpData[i]['title'].lower() != 'pending order':
+            del mdhpData[i]
+    return mdhpData
+
+def getSitesReserved(mdhpData):
+    for i in range(1, len(mdhpData) + 1):
+        if 'reserved' not in mdhpData[i]['title'].lower():
+            del mdhpData[i]
+    return mdhpData
+
+def getSitesPendingOrderOrReserved(mdhpData):
+    return {**getSitesPendingOrder(mdhpData.copy()),**getSitesReserved(mdhpData)}
+
+
 x = mdd.millionDollarData
 
-print(countSitesWithStatusCode(x, 200))
-print(pprint.pformat(statusCodeSummary(x)))
+#print(countSitesWithStatusCode(x, 200))
+#print(pprint.pformat(statusCodeSummary(x)))
 
+#generateHeatmap(x)
+#print(pprint.pformat(getSitesReserved(x)))
+#print(pprint.pformat(getSitesPendingOrder(x)))
 
-generateHeatmap(x)
+#TODO
+# print(pprint.pformat(getStatusCodeSummaryCounter(x)))
+
+print(pprint.pformat(getSitesPendingOrderOrReserved(x)))
